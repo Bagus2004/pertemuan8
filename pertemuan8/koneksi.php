990f2d32da2682a235bbe11ae042e239a45a5e3a
Bagus2004 <145316039+Bagus2004@users.noreply.github.com>
2023-11-20 12:46:38 +0700

    pertemuan8

---
+++ pertemuan8/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "pertamuan8";
+
+$koneksi = mysqli_connect("localhost","root","", "pertamuan8"); 
+// -->>>> "server", "user", "password","nama database"
+?>
